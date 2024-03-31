4cfe1cafb492cabc769f0dd0225abd1e6325bde4
Kir4R00t <miki4sa@interia.pl>
2024-03-31 22:00:26 +0200

    Initial commit

---
+++ src/main/java/me/kirar00t/kiraessentialz/KiraEssentialz.java
@@ -0,0 +1,16 @@
+package me.kirar00t.kiraessentialz;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KiraEssentialz extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
